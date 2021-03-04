0862ce2f2bec326e229625205067a77938527124
578350 <578350@W49L203S07.sd61.bc.ca>
2021-03-04 11:19:26 -0800

    Route Methods

--- Route.java
+++ Route.java
@@ -1,4 +1,29 @@
 
 public class Route {
-
+	Route(){
+		
+	}
+	
+	public int totalCost(){
+		return 0;
+		
+	}
+	
+	public int totalDistance(){
+		return 0;
+		
+	}
+	
+	public int totalSteps(){
+		return 0;
+		
+	}
+	
+	public void addLeg(Leg added){
+		
+	}
+	
+	public String toString() {
+		return "sadsad";
+	}
 }
