987afcc898f7d59b727e07ceae27c1954edc882d
ToodlesAndOodles <80061950+ToodlesAndOodles@users.noreply.github.com>
2021-03-04 10:06:14 -0800

    Add files via upload

---
+++ BikeLeg.java
@@ -0,0 +1,10 @@
+
+public class BikeLeg extends Leg{
+	Location origin;
+	Location destination;
+	int distance;
+	String days;
+	public BikeLeg() {
+		super(origin, destination, distance, days, 1);
+	}
+}
