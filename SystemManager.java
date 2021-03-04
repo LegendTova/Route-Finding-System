a31c83146454daf2454549a7cd42f72558c21ecf
ToodlesAndOodles <80061950+ToodlesAndOodles@users.noreply.github.com>
2021-03-04 10:40:33 -0800

    Update SystemManager.java

--- SystemManager.java
+++ SystemManager.java
@@ -1,8 +1,25 @@
-
+import java.util.*;
 public class SystemManager {
-	public static void addLocation() {
 	
+	public static ArrayList<Location> locations = new ArrayList<Location>();
+	public static ArrayList<Leg> legs = new ArrayList<Leg>();
+	public static void addLocation(Location l) {
+		locations.add(l);
 	}
+	public static void addLeg(Leg l){
+		legs.add(l);
+	}
+	public static String findLocation(String location) {
+		return "";
+	}
+	public static Route findCheapestRoute() {
+		return null;
+	}
+	public static Route findMinStepsRoute() {
+		return null;
+	}
+	public static Route findMinKmRoute() {
+		return null;
+	}
+	
 }
-
-
