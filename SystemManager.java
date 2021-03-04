14c8e8d60462bda9c02c3b562e4d69d04bf4e52d
ToodlesAndOodles <80061950+ToodlesAndOodles@users.noreply.github.com>
2021-03-04 11:14:34 -0800

    Update SystemManager.java

--- SystemManager.java
+++ SystemManager.java
@@ -3,14 +3,29 @@ public class SystemManager {
 	
 	public static ArrayList<Location> locations = new ArrayList<Location>();
 	public static ArrayList<Leg> legs = new ArrayList<Leg>();
-	public static void addLocation(Location l) {
+	public void printSystemDetails(){
+		System.out.println("List of Locations:");
+		for(int i = 0;i < locations.size();i++) {
+			System.out.println(printLocation(locations.get(i)));
+		}
+		System.out.println("List of Legs:");
+		for(int i = 0;i < legs.size();i++) {
+			System.out.println(printLeg(legs.get(i)));
+		}
+	}
+	public void addLocation(Location l) {
 		locations.add(l);
 	}
-	public static void addLeg(Leg l){
+	public void addLeg(Leg l){
 		legs.add(l);
 	}
-	public static String findLocation(String location) {
-		return "";
+	public static Location findLocation(String location) {
+		for(int i = 0;i < locations.size();i++) {
+			if(locations.get(i).name.equals(location)) {
+				return locations.get(i);
+			}
+		}
+		return null;
 	}
 	public static Route findCheapestRoute() {
 		return null;
@@ -21,5 +36,10 @@ public class SystemManager {
 	public static Route findMinKmRoute() {
 		return null;
 	}
-	
+	public static String printLocation(Location l) {
+		return l.name;
+	}
+	public static String printLeg(Leg l) {
+		return printLocation(l.origin) + " to " + printLocation(l.destination) + ". A trip of " + l.distance + "km with a cost of " + l.costPerKm + "$ per km";
+	}
 }
