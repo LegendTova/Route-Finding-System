7ca2fca80482c4576de9ea277fe4ad9d2ce30163
LegendTova <tdcmax10@gmail.com>
2021-03-05 15:48:34 -0800

    First FloydArray

--- SystemManager.java
+++ SystemManager.java
@@ -1,6 +1,8 @@
 import java.util.*;
 public class SystemManager {
 	
+	final static int INF = 9999;
+	
 	public static ArrayList<Location> locations = new ArrayList<Location>();
 	public static ArrayList<Leg> legs = new ArrayList<Leg>();
 	public void printSystemDetails(){
@@ -30,8 +32,10 @@ public class SystemManager {
 	}
 	public static Route findCheapestRoute(Location origin, Location destination, String day) {
 		double [] [] costPerStep = new double [locations.size()] [locations.size()];
+		boolean hasLeg;
 		
 		for(int i = 0;i < costPerStep.length;i++) {
+			hasLeg = false;
 			for(int j = 0;j < costPerStep[i].length;j++) {
 				if(j == i) {
 					costPerStep[i][j] = 0;
@@ -40,7 +44,13 @@ public class SystemManager {
 					if((legs.get(k).getOrigin()).equals(locations.get(i)) && (legs.get(k).getDestination()).equals(locations.get(j))) {
 						costPerStep[i][j] = legs.get(k).getDistance() * legs.get(k).getCost();
 						costPerStep[j][i] = legs.get(k).getDistance() * legs.get(k).getCost();
+						hasLeg = true;
 					}
+				}
+				
+				if(hasLeg == false) {
+					costPerStep[i][j] = INF;
+					costPerStep[j][i] = INF;
 				}
 			}
 		}
@@ -59,4 +69,29 @@ public class SystemManager {
 	public static String printLeg(Leg l) {
 		return printLocation(l.origin) + " to " + printLocation(l.destination) + ". A trip of " + l.distance + "km with a cost of " + l.costPerKm + "$ per km";
 	}
+	
+	// Floyd Warshall Algorithm in Java
+
+	    // Implementing floyd warshall algorithm
+	    public void floydWarshall(int graph[][]) {
+	    	int V = locations.size();
+	    	
+	        double matrix[][] = new double[V][V];
+	        int i, j, k;
+
+	    for (i = 0; i < V; i++) {
+	        for (j = 0; j < V; j++) {
+	            matrix[i][j] = graph[i][j];
+	        }
+	    }
+	    // Adding vertices individually
+	    for (k = 0; k < V; k++) {
+	        for (i = 0; i < V; i++) {
+	            for (j = 0; j < V; j++) {
+	                if (matrix[i][k] + matrix[k][j] < matrix[i][j])
+	                    matrix[i][j] = matrix[i][k] + matrix[k][j];
+	            }
+	        }
+	    }
+	  }
 }
